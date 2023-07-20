aff9a46937baf2ba81108a1799a72b4ea77784b7
dvcorreia1984 <victor@onsleer.co.za>
2023-07-20 15:04:56 +0200

    Fix EsLint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
     "env": {
         "browser": true,
+        "commonjs": true,
         "es2021": true
     },
-    "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        "ecmaVersion": 12
     },
     "rules": {
     }
