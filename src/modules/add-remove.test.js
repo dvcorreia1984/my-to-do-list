13631b8f1e3f157ba49cfd29239ceb577c39fa42
dvcorreia1984 <victor@onsleer.co.za>
2023-07-27 22:13:16 +0200

    Updated add-remove.test.js file

--- src/modules/add-remove.test.js
+++ src/modules/add-remove.test.js
@@ -1,6 +1,7 @@
 // add-remove.test.js
 
-import { addTask, Task } from './add-remove';
+import { before } from 'lodash';
+import { addTask, removeTask, Task } from './add-remove';
 
 // Test the addTask function
 describe('addTask', () => {
@@ -40,27 +41,38 @@ describe('addTask', () => {
 // Test the removeTask function
 describe('removeTask', () => {
   beforeEach(() => {
-    // Clear tasks before each test
     Task.tasks = [];
   });
 
-  test('should remove task at index', () => {
+  test('should remove task', () => {
     const initialTasks = [
-      new Task(false, 'Existing Task', 1),
-      new Task(false, 'First Task', 2),
+      new Task(false, 'First Task', 1),
+      new Task(false, 'Second Task', 2),
     ];
-    const expectedTasks = [new Task(false, 'Existing Task', 1)];
-    const result = Task.removeTask(1);
+    const expectedTasks = [new Task(false, 'Second Task', 1)];
+    const result = removeTask(1, initialTasks);
     expect(result).toEqual(expectedTasks);
   });
 
-  test('should update index of tasks after removal', () => {
+  // should remove correct task
+  test('should remove correct task', () => {
     const initialTasks = [
-      new Task(false, 'Existing Task', 1),
-      new Task(false, 'First Task', 2),
+      new Task(false, 'First Task', 1),
+      new Task(false, 'Second Task', 2),
     ];
-    const expectedTasks = [new Task(false, 'Existing Task', 1)];
-    const result = Task.removeTask(1);
+    const expectedTasks = [new Task(false, 'First Task', 1)];
+    const result = removeTask(2, initialTasks);
+    expect(result).toEqual(expectedTasks);
+  });
+
+  test('should update index after task removal correctly', () => {
+    const initialTasks = [
+      new Task(false, 'First Task', 1),
+      new Task(false, 'Second Task', 2),
+      new Task(false, 'Third Task', 3),
+    ];
+    const expectedTasks = [new Task(false, 'Second Task', 1), new Task(false, 'Third Task', 2)];
+    const result = removeTask(1, initialTasks);
     expect(result).toEqual(expectedTasks);
   });
 });
